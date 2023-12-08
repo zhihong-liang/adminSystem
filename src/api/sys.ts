f2f6795bf78eda1e46109db83740af6cbcb90dac
xiaohanzhu000 <316518966@qq.com>
2023-12-08 10:47:13 +0800

    升级 element plus

--- src/api/sys.ts
+++ src/api/sys.ts
@@ -49,3 +49,26 @@ export const dictDel = (data: string | number): Promise<Res> => axios.delete('/a
 // 根据数据字典类型查询所有的数据字典(无分页,查多个字典)
 export const dictAllList = (data: string[]): Promise<Res> =>
   axios.post('/api/admin/dict/getOptionsList', data)
+
+/**
+ * 角色
+*/
+export interface RoleType {
+  status?: string // 状态
+  name?: string // 名称
+  description?: string // 描述
+  menuIds?: string[] // 菜单ids
+}
+
+// 查询角色列表
+export const sysRoleList = (data: ListReq<RoleType>): Promise<ListRes> =>
+  axios.post('/api/admin/sysRole/list', data)
+
+// 新增角色
+export const sysRoleAdd = (data: SubDict): Promise<Res> => axios.post('/api/admin/sysRole/add', data)
+
+// 修改角色
+export const sysRoleEdit = (data: SubDict): Promise<Res> => axios.put('/api/admin/sysRole/edit', data)
+
+// 删除角色
+export const sysRoleRemove = (ids: string | number): Promise<Res> => axios.delete('/api/admin/sysRole/remove/' + ids)
--- src/views/sys/role/index.vue
+++ src/views/sys/role/index.vue
@@ -1,5 +1,11 @@
 <template>
   <CnSearch v-bind="search" :model="params" @search="handleQuery()" @reset="handleQuery()" />
+  <div class="role-list">
+    <el-card class="box-card" v-for="item in 12">
+      <div>card</div>
+      <template #footer>Footer cdfdfdfdfontent</template>
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,10 +15,23 @@ import CnSearch from '@/components/cn-page/CnSearch.vue'
 // 搜索
 const inited = ref(false)
 const search = ref({
-  items: [{ label: '主类', prop: 'type', component: 'input' }]
+  items: [
+    { label: '主类', prop: 'type', component: 'input' },
+    { label: '角色状态', prop: 'status', component: 'select' }
+  ]
 })
 const params = ref()
 const handleQuery = () => {}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.role-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  .box-card {
+    width: 200px;
+    height: 180px;
+  }
+}
+</style>
