a8c3165ffc4af27d0625251a753d43416a2671d6
liyp <liyp@createw.com>
2023-12-20 17:14:36 +0800

    feat：单位管理新增已选授权信息

---
+++ src/views/user/child/getAuth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>已选权限</div>
+  <CnTable v-bind="tableProp" style="margin:0 0 15px" />
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, defineProps, PropType, watch } from 'vue'
+import CnTable from '@/components/cn-page/CnTable.vue'
+
+const props = defineProps({
+  data: {
+    type: Array as PropType<any[]>,
+    default: () => []
+  }
+})
+
+const tableProp = reactive<CnPage.TableProps<User>>({
+  columns: [
+    { type: 'index', label: '序号', width: 60 },
+    { label: '区域', prop: 'areaName' },
+    { label: '授权', prop: 'authName', dict: 'DATA_PERMISSION_POLICY' },
+  ],
+  data: props.data
+})
+
+watch(() => props.data, (val) => {
+  tableProp.data = val
+})
+
+</script>
